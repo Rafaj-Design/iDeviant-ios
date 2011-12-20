7e16816e87abb3023becb95f46e9ebf1a6ced1c4
macralik <filipkralik@gmail.com>
2011-12-20 18:51:22 +0100

    image detail
    fix app crash when emailit

--- Classes/IDImageDetailViewController.m
+++ Classes/IDImageDetailViewController.m
@@ -336,7 +336,9 @@
     [mc setSubject:[NSString stringWithFormat:@"iDeviant"]];
     [mc setMessageBody:@"\n\n\n\nStickerTag app by Fuerte International UK - http://www.fuerteint.com/" isHTML:NO];
     [mc setMessageBody:@"</br></br></br></br>iDeviant app by <a href='http://www.fuerteint.com/'>Fuerte International UK</a>" isHTML:YES];
-    [mc addAttachmentData:UIImagePNGRepresentation(self.currentImage) mimeType:@"jpeg/png" fileName:[NSString stringWithFormat:@"%@.png", self.navigationController.title]];
+    if (self.currentImage) {
+        [mc addAttachmentData:UIImagePNGRepresentation(self.currentImage) mimeType:@"jpeg/png" fileName:[NSString stringWithFormat:@"%@.png", self.navigationController.title]];
+    }
     //[mc addAttachmentData:self.currentImage mimeType:@"image/png" fileName:[NSString stringWithFormat:@"%@.png", self.navigationController.title]];
     //self.currentImage = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"logo" ofType:@"png"]];
     //[mc addAttachmentData:self.currentImage mimeType:@"image/png" fileName:@"Fuerte_International_UK.png"];
