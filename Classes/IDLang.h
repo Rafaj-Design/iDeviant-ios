d1cc1fb436c9941ad7fe98d0dd5a047eb07eb567
Adam Horacek <adam.horacek@me.com>
2012-01-17 17:01:22 +0100

    adding files

---
+++ Classes/IDLang.h
@@ -0,0 +1,13 @@
+//
+//  IDLang.h
+//  iDeviant
+//
+//  Created by Adam Horacek on 17.01.12.
+//  Copyright (c) 2012 Fuerte International. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IDLang : NSObject
+
+@end
---
+++ Classes/IDLang.m
@@ -0,0 +1,13 @@
+//
+//  IDLang.m
+//  iDeviant
+//
+//  Created by Adam Horacek on 17.01.12.
+//  Copyright (c) 2012 Fuerte International. All rights reserved.
+//
+
+#import "IDLang.h"
+
+@implementation IDLang
+
+@end
