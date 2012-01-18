169d63798fbfaa5ef6febc6db10340f691f98ea3
Adam Horacek <adam.horacek@me.com>
2012-01-18 10:24:24 +0100

    adding files

---
+++ Classes/IDFavoriteCategoriesTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  IDFavoriteCategoriesTableViewCell.h
+//  iDeviant
+//
+//  Created by Adam Horacek on 18.01.12.
+//  Copyright (c) 2012 Fuerte International. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IDFavoriteCategoriesTableViewCell : UIViewController
+
+@end
